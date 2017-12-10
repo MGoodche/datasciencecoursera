0906376fefe69632ac0802d5949b114a1c6b49e7
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2017-12-10 10:32:28 +0100

    Update run_analysis.R

--- DataCleaning/Project/run_analysis.R
+++ DataCleaning/Project/run_analysis.R
@@ -2,5 +2,7 @@
 ## Step 1.1: Download zip file from website
 if(!file.exists("./data")) dir.create("./data") ## Check existence of directory and create if doesn't exist
 file <- url "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download(url, dest="data.zip") ## Define the folder where the zip file should be unzipped
-unzip ("data.zip") ## Unzip the file 
+download(url, dest="./data/datafile.zip", mode="wb") ## Define the folder where the zip file should be unzipped
+unzip ("data.zip",exdir = "./data") ## Unzip the file 
+
+## To see the files, use dir() command
