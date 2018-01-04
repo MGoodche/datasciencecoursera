22ac60c8c7f495a3f5a0c468568e933a9f14b1cd
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2018-01-04 11:49:58 +0100

    Update plot3.R

--- ExploratoryDataAnalysis/Project_Week4/plot3.R
+++ ExploratoryDataAnalysis/Project_Week4/plot3.R
@@ -1,3 +1,34 @@
 # Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen 
 # decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 
 # plotting system to make a plot answer this question.
+
+# 1. Loading provided datasets
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# 2. To see information about the structure of the objects.
+str(NEI)
+str(SCC)
+
+# 3. Unique Function returns a vector, data frame or array like x but with duplicate elements/rows removed.
+# Thanks to this we check the types.
+unique(NEI$type)
+ # [1] "POINT"    "NONPOINT" "ON-ROAD"  "NON-ROAD"
+
+# 4. Subset data from Baltimore City
+baltimorecity <- subset(NEI, fips == "24510")
+
+#head(baltimorecity)
+#        fips      SCC Pollutant Emissions  type year
+#114288 24510 10100601  PM25-PRI     6.532 POINT 1999
+#114296 24510 10200601  PM25-PRI    78.880 POINT 1999
+#114300 24510 10200602  PM25-PRI     0.920 POINT 1999
+#114308 24510 30100699  PM25-PRI    10.376 POINT 1999
+#114325 24510 30183001  PM25-PRI    10.859 POINT 1999
+#114329 24510 30201599  PM25-PRI    83.025 POINT 1999
+
+#5. Now, we want to calculate the total sum of the emissions of Baltimore City but broken by years, so we will use the tapply() function
+totalemissions2 <- tapply(baltimorecity$Emissions, baltimorecity$year, sum)
+
+
+
