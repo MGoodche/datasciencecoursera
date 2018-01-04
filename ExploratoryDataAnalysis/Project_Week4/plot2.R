6bdcf00a64635c0b7539cf54ac95fb7e3e002f65
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2018-01-04 11:26:36 +0100

    Update plot2.R

--- ExploratoryDataAnalysis/Project_Week4/plot2.R
+++ ExploratoryDataAnalysis/Project_Week4/plot2.R
@@ -1,4 +1,8 @@
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting
 # system to make a plot answering this question.
 
-# 1. 
+# 1. Loading provided datasets
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# 2. 
