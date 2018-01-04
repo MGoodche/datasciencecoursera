8f9138bb058cf81b4b86a2f115acd2c1d7e8d1bc
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2018-01-04 10:42:48 +0100

    Update Question1.R

--- ExploratoryDataAnalysis/Project_Week4/Question1.R
+++ ExploratoryDataAnalysis/Project_Week4/Question1.R
@@ -1,5 +1,11 @@
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? Using the base plotting system, make a plot showing 
 # the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
-# Loading provided datasets
+# 1. Loading provided datasets
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+# 2. To see information about the structure of the objects.
+str(NEI)
+str(SCC)
+
+# 3.
