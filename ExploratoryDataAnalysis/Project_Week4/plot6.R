ecb5640a0406fe485d201c82ef6e6e2481d0cfa6
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2018-01-05 11:42:31 +0100

    Update plot6.R

--- ExploratoryDataAnalysis/Project_Week4/plot6.R
+++ ExploratoryDataAnalysis/Project_Week4/plot6.R
@@ -1,1 +1,13 @@
+# Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County,
+# California (fips == "06037"). Which city has seen greater changes over time in motor vehicle emissions?
 
+# 1. Loading provided datasets
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# 2. To see information about the structure of the objects.
+str(NEI)
+str(SCC)
+
+# 3. 
+baltimore_angeles <- subset(NEI, (fips == "24510" | fips == "06037") & type =="ON-ROAD")
