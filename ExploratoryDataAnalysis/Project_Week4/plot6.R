60fab6f5edd8f3e4bd0ac090bc985410c52ff8ba
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2018-01-05 12:49:10 +0100

    Update plot6.R

--- ExploratoryDataAnalysis/Project_Week4/plot6.R
+++ ExploratoryDataAnalysis/Project_Week4/plot6.R
@@ -12,10 +12,18 @@ str(SCC)
 # 3.  Subset data from Baltimore City and Los Angeles 
 baltimore_angeles <- subset(NEI, (fips == "24510" | fips == "06037") & type =="ON-ROAD")
 
+# 4. Rename the cities using plyr package and the function rename
+library(plyr)
+baltimore_angeles <- rename(baltimore_angeles, c("fips"="City"))
+baltimore_angeles$City <- factor(baltimore_angeles$City, levels=c("06037", "24510"), labels=c("Los Angeles", "Baltimore"))
 
-ggplot(baltimore_angeles, aes(x=factor(year), y=Emissions, fill=city)) +
- geom_bar(aes(fill=year),stat="identity") +
- guides(fill=FALSE) + theme_bw() +
- labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
- labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
- 
+# 5. Plot the result
+ ggplot(baltimore_angeles, aes(x = factor(year), y = Emissions, fill = City)) +
+        geom_bar(stat = "identity", width = 0.7) +
+        facet_grid(.~City) + 
+        labs(x = "Year", y = "Emissions (Tons)", title = "Comparison of Motor Vehicle Related Emissions: Baltimore City and Los Angeles (1999 - 2008)") +
+        theme(plot.title = element_text(size = 14),
+              axis.title.x = element_text(size = 12),
+              axis.title.y = element_text(size = 12),
+              strip.text.x = element_text(size = 12)) +
+        theme_light()
