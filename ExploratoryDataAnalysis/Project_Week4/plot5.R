43bb6adeca8ad1e2066e1bd5829c6878ed61e880
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2018-01-05 12:56:56 +0100

    Update plot5.R

--- ExploratoryDataAnalysis/Project_Week4/plot5.R
+++ ExploratoryDataAnalysis/Project_Week4/plot5.R
@@ -13,7 +13,16 @@ baltimore2 <- aggregate(Emissions ~ year, baltimore2, sum)
 
 # 4. Plot the results
 library(ggplot2)
-ggplot(data=baltimore2, aes(x=year, y=Emissions)) + geom_line(linetype="dashed", size = 1.5) + geom_point( size=5, shape=21, fill="blue") + xlab("Year") + ylab("Emissions (tons)") + ggtitle("Motor Vehicle PM2.5 Emissions in Baltimore")
+ggplot(baltimore2, aes(x = year, y = Emissions)) +
+        geom_line(linetype="dashed", size = 1.5) +
+        labs(x = "Year", y = "Emissions (Tons)", title = "Motor Vehicle PM2.5 Emissions in Baltimore (1999 - 2008)") +
+        theme(plot.title = element_text(size = 14),
+              axis.title.x = element_text(size = 12),
+              axis.title.y = element_text(size = 12),
+              strip.text.x = element_text(size = 12)) +
+        theme_light()
+
+# ggplot(data=baltimore2, aes(x=year, y=Emissions)) + geom_line(linetype="dashed", size = 1.5) + geom_point( size=5, shape=21, fill="blue") + xlab("Year") + ylab("Emissions (tons)") + ggtitle("Motor Vehicle PM2.5 Emissions in Baltimore")
 
 # 5. Save the picture
 dev.copy(png, file = "plot5.png" ,width = 500, height = 500)  
