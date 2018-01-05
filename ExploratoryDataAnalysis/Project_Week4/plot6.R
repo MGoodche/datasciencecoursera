ca076561b2ebd49eb47dc824eb4f4f1dad51ced2
Miriam Pavón Buenache <mirriam.upm@gmail.com>
2018-01-05 11:51:30 +0100

    Update plot6.R

--- ExploratoryDataAnalysis/Project_Week4/plot6.R
+++ ExploratoryDataAnalysis/Project_Week4/plot6.R
@@ -9,5 +9,13 @@ SCC <- readRDS("Source_Classification_Code.rds")
 str(NEI)
 str(SCC)
 
-# 3. 
+# 3.  Subset data from Baltimore City and Los Angeles 
 baltimore_angeles <- subset(NEI, (fips == "24510" | fips == "06037") & type =="ON-ROAD")
+
+
+ggplot(baltimore_angeles, aes(x=factor(year), y=Emissions, fill=city)) +
+ geom_bar(aes(fill=year),stat="identity") +
+ guides(fill=FALSE) + theme_bw() +
+ labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+ labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+ 
